75569a4e13f872e8df0e56cd8f1d9f922edb7ab7
Miral Savera <miralsavera2702@gmail.com>
2023-08-06 13:59:09 +0530

    Fix : Authentication when server start issue fixed

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
-const mongouri = "mongodb+srv://priteshbhatiya52:um5m8yav61ytlmks@cluster0.gfohlpd.mongodb.net/glsevents?retryWrites=true&w=majority";
+const mongouri = "mongodb+srv://priteshbhatiya52:uM5M8YaV61ytlmKS@cluster0.gfohlpd.mongodb.net/Project-3?retryWrites=true&w=majority";
 mongoose.connect(mongouri).then(()=>{console.log("Connect")})
 .catch((e)=>{console.log(e)});
