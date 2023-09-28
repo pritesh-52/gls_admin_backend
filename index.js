83264e20793674d33d5051fd45a828cc47bd6136
Miral Savera <miralsavera2702@gmail.com>
2023-09-28 21:11:58 +0530

    cors enable

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-//var cors = require('cors')
+var cors = require('cors')
 const app = express()
 const port = 8000
 require("./db");
 
-//app.use(cors())
+app.use(cors())
 app.use(express.json())
 
 //connectToMongo();
